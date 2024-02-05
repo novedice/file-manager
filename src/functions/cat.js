6169cb87aac97082f71ab217f4fe222b79603c24
novedice <Natalia.kitesurf@gmail.com>
2024-02-05 23:58:04 +0000

    feat: implement cat function

--- src/functions/cat.js
+++ src/functions/cat.js
@@ -1,8 +1,18 @@
 import { showCurDir } from "./showCurrentDirectory.js";
+import fs from "fs";
+import { resolve } from "path";
 
 export const readAndPrint = async (input) => {
-  console.log("cat ", input.split(" ").slice(1));
   try {
+    const reader = fs.createReadStream(resolve(input.split(" ").slice(1)[0]), {
+      encoding: "utf8",
+    });
+    reader.on("data", (chunk) => {
+      console.log(chunk);
+    });
+    reader.on("error", () => {
+      console.log("Operation failed");
+    });
     showCurDir();
   } catch {
     console.log("Operation failed");
