a292fd1946bb65ec97bbbfe717e90212103df4f0
novedice <Natalia.kitesurf@gmail.com>
2024-02-05 23:51:03 +0000

    feat: implement rename function

--- src/functions/rn.js
+++ src/functions/rn.js
@@ -1,8 +1,17 @@
 import { showCurDir } from "./showCurrentDirectory.js";
+import { resolve } from "path";
+import path from "path";
+import fsPromises from "fs/promises";
 
 export const renameFile = async (input) => {
-  console.log("rename", input.split(" ").slice(1));
+  const args = input
+    .split(" ")
+    .slice(1)
+    .filter((v) => v !== " ");
   try {
+    const thePath = resolve(args[0]);
+    const theDir = path.dirname(thePath);
+    await fsPromises.rename(thePath, path.join(theDir, args[1]));
     showCurDir();
   } catch {
     console.log("Operation failed");
